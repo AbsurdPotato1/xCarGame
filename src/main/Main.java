ae4f0afca93437949c14e55409c3422791f05750
AbsurdPotato1 <ethanwang0930@gmail.com>
2024-04-17 12:51:46 -0700

    main.Main essentials created src/main package created

---
+++ src/main/Main.java
@@ -0,0 +1,22 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args){
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("xCar");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack(); // fits panel to size
+
+        window.setLocationRelativeTo(null); // center of the screen woot woot
+        window.setVisible(true);
+
+        gamePanel.startGameThread();
+    }
+}
